b19d5bb2afc9659818329aa391a3aad79fe72488
nguyen09033-maker <nguyen09033@gmail.com>
2026-06-19 00:48:12 +0700

    fix

--- Assets/Script/Sound.cs
+++ Assets/Script/Sound.cs
@@ -1,13 +1,23 @@
+using System;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Sound : MonoBehaviour
 {
     public AudioSource sfxAudio;
     public AudioClip lineclear;
     public AudioClip placeBlock;
+    [SerializeField] private AudioMixer my_mixer;
+    [SerializeField] private Slider slider;
     public void play_sfx(AudioClip audio)
     {
         sfxAudio.clip=audio;
         sfxAudio.PlayOneShot(audio);
+    }
+    public void SetVolumesfx()
+    {
+       float sfxvolume= slider.value;
+       my_mixer.SetFloat("sfx",sfxvolume);
     }
 }
