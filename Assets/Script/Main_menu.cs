ff9f4fd8b2d43f37b41da63b1d1e44164e8068ba
HOÀNG QUÂN <nguyen09033@gmail.com>
2026-07-22 02:18:31 +0700

    update_main_menu

---
+++ Assets/Script/Main_menu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Main_menu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Play_Scene");
+        Debug.Log("nhan play");
+    }
+}
