1d03817ce736ba3ccc2c6b1fe0c047f90ab8e164
HOÀNG QUÂN <nguyen09033@gmail.com>
2026-07-30 01:36:16 +0700

    dùng playerprefab lưu thông số âm thanh

--- Assets/Script/setting,button,sound/Sound.cs
+++ Assets/Script/setting,button,sound/Sound.cs
@@ -10,6 +10,17 @@ public class Sound : MonoBehaviour
     public AudioClip placeBlock;
     [SerializeField] private AudioMixer my_mixer;
     [SerializeField] private Slider slider;
+    float volume_current_value;
+    
+    void Start()
+    {
+        volume_current_value=PlayerPrefs.GetFloat("Volume",1.0f);
+        my_mixer.SetFloat("sfx",volume_current_value);
+        if(slider!=null)
+        {
+            slider.value=Mathf.Pow(10,volume_current_value/20);
+        }
+    }
     public void play_sfx(AudioClip audio)
     {
         sfxAudio.clip=audio;
@@ -18,6 +29,8 @@ public class Sound : MonoBehaviour
     public void SetVolumesfx()
     {
        float sfxvolume= Mathf.Clamp(slider.value,0.00001f,20f);
-       my_mixer.SetFloat("sfx",Mathf.Log10(sfxvolume)*20);
+       volume_current_value=Mathf.Log10(sfxvolume)*20;
+       my_mixer.SetFloat("sfx",volume_current_value);
+       PlayerPrefs.SetFloat("Volume",volume_current_value);
     }
 }
